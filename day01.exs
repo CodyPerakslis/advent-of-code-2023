1f117c6623028fc6d81976fe92f09cc8697d72c7
CodyPerakslis <codyperakslis@gmail.com>
2023-12-01 16:47:48 -0500

    get first and last  digits of a string

--- day01.exs
+++ day01.exs
@@ -1,6 +1,6 @@
 defmodule Day01 do
-  def getFirstNumber(a) do
-    case a do 
+  def getFirstNumber(str) do
+    case str do 
         "" -> {:error, "No number found"}
         "1" <> rest -> {1, rest}
         "2" <> rest -> {2, rest}
@@ -11,7 +11,21 @@ defmodule Day01 do
         "7" <> rest -> {7, rest}
         "8" <> rest -> {8, rest}
         "9" <> rest -> {9, rest}
-        <<first::utf8, rest::binary>> -> getFirstNumber(rest)
+        <<_, rest::binary>> -> getFirstNumber(rest)
+    end
+  end
+
+  def getFirstAndLastNumbers({first, currentLast, rest}) do
+    case getFirstNumber(rest) do
+      {:error, _} -> {first, currentLast}
+      {number, rest} -> getFirstAndLastNumbers({first, number, rest})
+    end
+  end
+
+  def getFirstAndLastNumbers(str) do 
+    case getFirstNumber(str) do
+      {:error, _} -> {:error, "No number found"}
+      {number, rest} -> getFirstAndLastNumbers({number, number, rest})
     end
   end
 end
@@ -19,7 +33,7 @@ end
 
 IO.puts("Hello, world!")
 
-{number, rest} = Day01.getFirstNumber("a1k2x3o")
+{number, rest} = Day01.getFirstAndLastNumbers("a4eu3shs2o")
 
 IO.puts(number)
 IO.puts(rest)
