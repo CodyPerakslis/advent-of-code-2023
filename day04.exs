0ea5287464c17443a8325997b6587fa8eb99f204
CodyPerakslis <codyperakslis@gmail.com>
2023-12-04 16:07:11 -0500

    solve day04 part 2

--- day04.exs
+++ day04.exs
@@ -21,6 +21,51 @@ defmodule Day04 do
             |> Enum.sum()
             |> IO.inspect()
     end
-end
 
-Day04.question1
+    def addWins(rest, {_, 0}) do 
+        rest
+    end
+
+    def addWins(rest, {0, _}) do 
+        rest
+    end
+
+    def addWins([], _) do
+        []
+    end
+
+    def addWins([{nWin, nCount} | rest], {win, count}) do 
+        [
+            {nWin, nCount + count} | 
+            addWins(rest, {win - 1, count})
+            ]
+    end
+
+    def setWins(wins) do 
+        setWins(wins, [])
+    end
+
+    def setWins([], result) do 
+        result
+    end
+
+    def setWins([next | rest], result) do 
+        addWins(rest, next)
+            |> setWins(result ++ [next])
+    end
+
+    def question2 do 
+        File.read!("day04.txt")
+            |> String.split("\n")
+            |> Enum.map(fn line -> String.split(line, ": ") end)
+            |> Enum.map(fn [_, numbers] -> String.split(numbers, " | ") end)
+            |> Enum.map(fn [lottery, guess] -> [sortNumbers(lottery), sortNumbers(guess)] end)
+            |> Enum.map(fn [lottery, guess] -> commonNumbers(lottery, guess) end)
+            |> Enum.map(fn set -> {MapSet.size(set), 1} end)
+            |> IO.inspect(label: "wins")
+            |> Day04.setWins()
+            |> IO.inspect(label: "counts")
+            |> Enum.reduce(0, fn {_, count}, acc -> acc + count end)
+            |> IO.inspect(label: "answer")
+    end
+end
