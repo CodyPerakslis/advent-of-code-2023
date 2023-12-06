be699d1878030a28a6f06b191a03d03cc598b821
CodyPerakslis <codyperakslis@gmail.com>
2023-12-05 20:53:19 -0500

    day05 incomplete part 2

--- day05.exs
+++ day05.exs
@@ -14,6 +14,44 @@ defmodule Day05 do
         end
     end
 
+    def beforeRange(start, range, target, _) do 
+        cond do 
+            start >= target  -> :none 
+            start + range <= target -> {start, range}
+            true -> {start, target - start}
+        end
+    end
+
+    def midRange(start, range, target, targetRange) do 
+        cond do 
+            start + range <= target -> :none
+            start >= target + targetRange -> :none 
+            start <= target -> {target, Enum.min([start + range, target + targetRange])}
+            true -> {start, Enum.min([start + range, target + targetRange])}
+        end
+    end
+
+    def afterRange(start, range, target, targetRange) do 
+        cond do 
+            start + range <= target + targetRange -> :none 
+            start >= target + targetRange -> {start, range}
+            true -> {target, start + range - (target + targetRange)}
+        end
+    end
+
+    def mapRange({seedStart, seedRange}, [destStart, srcStart, srcRange]) do 
+        {
+            beforeRange(seedStart, seedRange, srcStart, srcRange),
+            midRange(seedStart, seedRange, srcStart, srcRange)
+                |> (fn x -> case x do 
+                    :none -> :none 
+                    {start, range} -> {start + destStart - srcStart, range}
+                    end
+                end).(),
+            afterRange(seedStart, seedRange, srcStart, srcRange),
+        }
+    end
+
     def getValueLists(map, waiting) do 
         getValueLists(map, waiting, [], [])
     end
@@ -28,6 +66,44 @@ defmodule Day05 do
             {:found, key} -> getValueLists(map, remaining, misses, [{start, key} | finds])
         end
     end 
+
+    def maybeAdd(rangeList, range) do 
+        case range do 
+            :none -> rangeList
+            _ -> [range | rangeList]
+        end
+    end
+
+    def getValueLists2(map, waiting) do 
+        getValueLists2(map, waiting, [], [])
+    end
+
+    def getValueLists2(_, [], misses, finds) do 
+        {misses, finds}
+    end
+
+    def getValueLists2(map, [seed | remaining], misses, finds) do 
+        {r1, r2, r3 } = mapRange(seed, map)
+        IO.inspect({seed, map, r1, r2, r3})
+        getValueLists2(map, remaining, maybeAdd(misses, r1) |> maybeAdd(r3), maybeAdd(finds, r2))
+    end 
+
+    def travelMap2({seedValues, lines}) do 
+        travelMap2(seedValues, lines, [])
+    end
+
+    def travelMap2(waitingValues, [], foundValues) do 
+        waitingValues ++ foundValues
+    end
+
+    def travelMap2(waitingValues, [line | remainingLines], foundValues) do 
+        case String.ends_with?(line, "map:") do 
+            true -> travelMap2(waitingValues ++ foundValues, remainingLines, [])
+            false -> getNumbers(line) 
+                |> getValueLists2(waitingValues) 
+                |> (fn {misses, finds} -> travelMap2(misses, remainingLines, finds ++ foundValues) end).()
+        end
+    end
 
     def travelMap({seedValues, lines}) do 
         travelMap(seedValues, lines, [])
@@ -60,7 +136,29 @@ defmodule Day05 do
             |> Enum.min_by(fn {_, plot} -> plot end)
             |> IO.inspect(label: "minimum")
     end 
+
+    def getSeeds([], groups) do 
+        groups
+    end
+
+    def getSeeds([s1 | [s2 | seeds]], groups) do 
+        getSeeds(seeds, [{s1, s2} | groups])
+    end 
+
+
+    def question2 do 
+        File.read!("day05.test.txt")
+            |> String.split("\n")
+            |> Enum.filter(fn line -> line !== "" end)
+            |> (fn ["seeds: " <> seeds | maps] -> {
+                getNumbers(seeds) |> getSeeds([]), 
+                maps
+                } end).()
+            |> IO.inspect(label: "lines")
+            |> travelMap2()
+            |> IO.inspect(label: "mappings")
+    end 
 end 
 
 
-Day05.question1
+Day05.question2
